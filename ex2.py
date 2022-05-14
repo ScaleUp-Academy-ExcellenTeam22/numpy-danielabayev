26b3507bae86b5596880ba3ff727355c6a1b4525
daniel or abayev <dan5204@gmail.com>
2022-05-14 23:20:16 +0300

    ex2

---
+++ ex2.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def ex2() -> list:
+    """
+    NumPy program which create a vector of length 10 with values evenly distributed between 5 and 50.
+    :return: vector of the numbers.
+    """
+    vector = np.linspace(5, 49, 10)
+    return vector
+
+
+if __name__ == '__main__':
+    print(ex2())
