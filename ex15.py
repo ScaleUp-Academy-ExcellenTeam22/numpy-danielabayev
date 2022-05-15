a93a242229e12ae64add4d0855bc5086ff0982b8
daniel or abayev <dan5204@gmail.com>
2022-05-15 12:22:09 +0300

    ex15

---
+++ ex15.py
@@ -0,0 +1,17 @@
+import numpy
+import numpy as np
+
+
+def three_dimension() -> numpy.ndarray:
+    """
+    A NumPy program to create a three-dimension array with shape (300,400,5) and set to a variable. Fill the
+    array elements with values using unsigned integer (0 to 255).
+    :return: the array.
+    """
+    np.random.seed(32)
+    array = np.random.randint(0, 256, size=(300, 400, 5), dtype=np.uint8)
+    return array
+
+
+if __name__ == '__main__':
+    print(three_dimension())
